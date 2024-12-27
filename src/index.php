357a9a0dc384cdf4045b14a4c5a7d1e05b735e5d
yasser-aboufaris <azertyasser321@gmail.com>
2024-12-27 20:49:19 +0100

    fixing the classes bugs

--- src/index.php
+++ src/index.php
@@ -1,5 +1,3 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,10 +16,15 @@
     <?php
     include "../htmlCom/footer.php";
     include "../CLASSES/countries.php";
+    include "../CLASSES/cities.php"
     ?>
     <?php
     
     $Country = $newCountry->Read();
+    $newCity = new Cities($newConnection);
+    $city = $newCity->read() ;
+    
+    var_dump($city);
     foreach($Country as $row ){
         ?>
         
@@ -43,6 +46,13 @@
                             </a>
                             <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 ">
                                 delete
+                                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                </svg>
+                            </a>
+
+                            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 ">
+                                edit cities
                                 <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                                 </svg>
